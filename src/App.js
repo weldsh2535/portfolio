3c0ca17005c5b11ed3231dd188248967ec0095f1
weldsh2535 <74667144+weldsh2535@users.noreply.github.com>
2022-06-30 16:43:45 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Home from './Components/Home';
+import About from './Components/About'
+import Service from './Components/Service';
+import Project from './Components/Project';
+import Blog from './Components/Blog';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='home__bg'>
+        <Home/>
+        <About/>
+        <Service/>
+        <Project/>
+        <Blog/>
+        <Contact/>
+        <Footer/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Service.js
@@ -0,0 +1,146 @@
+import React from "react";
+import "./Service.css";
+
+function Service() {
+  return (
+    <div className="service component__space" id="Services">
+      <div className="heading">
+        <h1 className="heading">My Awesome Service</h1>
+        <p className="heading p__color">
+          There are many variations of passages of Weldsh Aweke available,
+        </p>
+        <p className="heading p__color">
+          but the majority have suffered alteration.
+        </p>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col__3">
+            <div className="service__box pointer">
+              <div className="icon">
+                <svg
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M2 16.1A5 5 0 0 1 5.9 20M2 12.05A9 9 0 0 1 9.95 20M2 8V6a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2h-6"></path>
+                  <line x1="2" y1="20" x2="2" y2="20"></line>
+                </svg>
+              </div>
+              <div className="service__meta">
+                <h1 className="service__text">UI/UX Design</h1>
+                <p className="p service__text p__color">
+                  I throw myself down among the
+                </p>
+                <p className="p service__text p__color">
+                  tall grass by the stream as I lie
+                </p>
+                <p className="p service__text p__color">close to the earth.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col__3">
+            <div className="service__box pointer">
+              <div className="icon">
+                <svg
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <polygon points="12 2 2 7 12 12 22 7 12 2"></polygon>
+                  <polyline points="2 17 12 22 22 17"></polyline>
+                  <polyline points="2 12 12 17 22 12"></polyline>
+                </svg>
+              </div>
+              <div className="service__meta">
+                <h1 className="service__text">Website Development</h1>
+                <p className="p service__text p__color">
+                  I throw myself down among the
+                </p>
+                <p className="p service__text p__color">
+                  tall grass by the stream as I lie
+                </p>
+                <p className="p service__text p__color">close to the earth.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col__3">
+            <div className="service__box pointer">
+              <div className="icon">
+                <svg
+                  stroke="currentColor"
+                  fill="none"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect>
+                  <line x1="8" y1="21" x2="16" y2="21"></line>
+                  <line x1="12" y1="17" x2="12" y2="21"></line>
+                </svg>
+              </div>
+              <div className="service__meta">
+                <h1 className="service__text">Mobile App Development</h1>
+                <p className="p service__text p__color">
+                  I throw myself down among the
+                </p>
+                <p className="p service__text p__color">
+                  tall grass by the stream as I lie
+                </p>
+                <p className="p service__text p__color">close to the earth.</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col__3">
+            <div className="service__box pointer">
+              <div className="icon">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-bug"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M4.355.522a.5.5 0 0 1 .623.333l.291.956A4.979 4.979 0 0 1 8 1c1.007 0 1.946.298 2.731.811l.29-.956a.5.5 0 1 1 .957.29l-.41 1.352A4.985 4.985 0 0 1 13 6h.5a.5.5 0 0 0 .5-.5V5a.5.5 0 0 1 1 0v.5A1.5 1.5 0 0 1 13.5 7H13v1h1.5a.5.5 0 0 1 0 1H13v1h.5a1.5 1.5 0 0 1 1.5 1.5v.5a.5.5 0 1 1-1 0v-.5a.5.5 0 0 0-.5-.5H13a5 5 0 0 1-10 0h-.5a.5.5 0 0 0-.5.5v.5a.5.5 0 1 1-1 0v-.5A1.5 1.5 0 0 1 2.5 10H3V9H1.5a.5.5 0 0 1 0-1H3V7h-.5A1.5 1.5 0 0 1 1 5.5V5a.5.5 0 0 1 1 0v.5a.5.5 0 0 0 .5.5H3c0-1.364.547-2.601 1.432-3.503l-.41-1.352a.5.5 0 0 1 .333-.623zM4 7v4a4 4 0 0 0 3.5 3.97V7H4zm4.5 0v7.97A4 4 0 0 0 12 11V7H8.5zM12 6a3.989 3.989 0 0 0-1.334-2.982A3.983 3.983 0 0 0 8 2a3.983 3.983 0 0 0-2.667 1.018A3.989 3.989 0 0 0 4 6h8z" />
+                </svg>
+              </div>
+              <div className="service__meta">
+                <h1 className="service__text">Testing</h1>
+                <p className="p service__text p__color">
+                  I throw myself down among the
+                </p>
+                <p className="p service__text p__color">
+                  tall grass by the stream as I lie
+                </p>
+                <p className="p service__text p__color">close to the earth.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Service;
